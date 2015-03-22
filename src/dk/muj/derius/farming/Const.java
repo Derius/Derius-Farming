5da0c97aa21faea43928e67cf9566350247e13d6
ulumulu1510 <beniheusser@gmail.com>
2015-03-22 11:27:22 +0100

    Reorder constants

--- src/dk/muj/derius/farming/Const.java
+++ src/dk/muj/derius/farming/Const.java
@@ -9,8 +9,6 @@ public class Const
 	public static final String BASENAME = "derius_farming";
 	public static final String BASENAME_ = BASENAME+"_";
 	
-	public static final String COLLECTION_MCONF = BASENAME_+"mconf";
-	
 	public static final String JSON_EXP_GAIN = "Exp Gain";
 	public static final String JSON_CAREFUL_HARVESTING = "Carefull Harvesting";
 	public static final String JSON_FERTILIZE_FIELD_MIN_LVL = "FertilizeField min level";
